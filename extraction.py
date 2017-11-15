9691211d137b121192fd85566b887c6fc830e36e
Amitesh <amitesh080295@gmail.com>
2017-11-15 15:20:29 +0530

    Update extraction.py

--- extraction.py
+++ extraction.py
@@ -1,24 +1,22 @@
 import pandas as pd
 import gzip
 import os
+import string
+import re
 
-#number of reviews
-
-num_reviews = 2000
-num_test = 6000
+stop = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
 
 def touch_test(df, folder):
-    for x in range(len(df.index[num_reviews+1:num_reviews+num_test])):
-		
+    for x in range(len(df.index[0:int(len(df)/2)])):
         filename = 'f' + folder + str(x) + '.txt'
-        with open("test" + "/" + folder + "/" + filename, 'w') as output_file:
-            output_file.write(df.reviewText[x])
+        with open("try" + "/" + folder + "/" + filename, 'w') as output_file:
+            output_file.write(''.join(df.reviewText[x]))
 
 def touch_train(df, folder):
-    for x in range(len(df.index[0:num_reviews])):
+    for x in range(len(df.index[int(len(df)/2):])):
         filename = 'f' + folder + str(x) + '.txt'
         with open("training" + "/" + folder + "/" + filename, 'w') as output_file:
-            output_file.write(df.reviewText[x])#int(len(df)/2) +
+            output_file.write(''.join(df.reviewText[int(len(df)/2) + x]))
 
 def make_folder(directory):
     folders = 5
@@ -39,13 +37,22 @@ def getDF(path):
     i += 1
   return pd.DataFrame.from_dict(df, orient='index')
 
+def remove_stop(row):
+    global stop
+    lis=[word for word in row["reviewText"].split(" ") if word not in stop]
+    string=""
+    for word in lis:
+        string=string+word+" "
+    return string[0:len(string)-1]
+
 df = getDF('reviews_Cell_Phones_and_Accessories_5.json.gz')
 new = df[['overall','reviewText']].copy()
+new['reviewText'] = new.apply(remove_stop,axis=1)
 
-if not os.path.exists("test"): os.makedirs("test")
+if not os.path.exists("try"): os.makedirs("try")
 if not os.path.exists("training"): os.makedirs("training")
 
-make_folder("test")
+make_folder("try")
 make_folder("training")
 
 all_one = new[new['overall'] == 1]
